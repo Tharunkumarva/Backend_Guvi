59cbb49ff3d71b6aa74430af2bded07ce4728582
Tharunkumarva <tharunkumarva@gmail.com>
2023-11-29 17:03:47 +0530

    json response

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+let notes = [
+  { id: 1, content: 'Tamilnadu girls is best', important: true },
+  { id: 2, content: 'kerala girls is best', important: true },
+  { id: 3, content: 'delhi girls is best', important: false }
+];
 
-const server = http.createServer((req,res)=>{
-    res.statuscode = 200;
-    res.setHeader('Content_type','text/plain');
-    res.end('Hello Worls\n');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(notes));
 });
 
-server.listen(port, hostname,()=>{
-   
-    console.log(`server running at http://${hostname}:${port}`);
+const hostname = '127.0.0.1';
+const port = 3001;
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
